3b439819b4157bef0417d66bf74190198ae51853
nossol <65157992+nossol@users.noreply.github.com>
2020-05-11 16:24:25 +0200

    Add files via upload

---
+++ Episode_5/Episode_5_index.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP LESSON</title>
+    <style>
+        header {
+            background: darkolivegreen;
+            padding: 2em;
+            text-align: center;
+        }
+        h1 {
+            color: white;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h1>
+        <?= $greeting; ?>
+    </h1>
+</header>
+</body>
+
+</html>
+
