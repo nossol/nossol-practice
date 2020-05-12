9ad79068906447c212c27ae8c9b11f36aabbd178
nossol <65157992+nossol@users.noreply.github.com>
2020-05-12 12:46:06 +0200

    Add files via upload

---
+++ Episode_7_Associative_Arrays/Episode_7.php
@@ -0,0 +1,19 @@
+<?php
+
+$tasks = [
+    [
+        'Title' => 'Watch Tutorials',
+        'Due' => 'Now!',
+        'Assigned to' => 'Oliver',
+        'Completed' => 'No'
+    ],
+    [
+        'Title' => 'Eat Breakfast',
+        'Due' => '9 AM',
+        'Assigned to' => 'Oliver',
+        'Completed' => 'Yes'
+    ]
+];
+
+require 'index.view.php';
+
